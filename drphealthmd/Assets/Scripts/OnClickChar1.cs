67ed6331263544e425e1bf39739334b1118ac9de
Joel Graves <jgraves1@ltu.edu>
2019-07-31 15:58:06 -0400

    Added Firebase and Working on Login Scene
    Added firebase SDK and started working on login screen.

---
+++ drphealthmd/Assets/Scripts/OnClickChar1.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class OnClickChar1 : MonoBehaviour
+{
+    GameObject SelectedText;
+    // Start is called before the first frame update
+    void Start()
+    {
+        SelectedText = GameObject.FindGameObjectWithTag("Selected");
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void OnClick()
+    {
+        SelectedText.GetComponent<Text>().text = "Character Selected: 1";
+    }
+}
+
