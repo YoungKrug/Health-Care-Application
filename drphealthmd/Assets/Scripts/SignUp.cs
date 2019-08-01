3baaec7e5e1c27a910855a5507590ecf3ff38ffd
Joel Graves <jgraves1@ltu.edu>
2019-08-01 11:38:39 -0400

    add more features to the authentication screen**Rebased

---
+++ drphealthmd/Assets/Scripts/SignUp.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class SignUp : MonoBehaviour
+{
+    public GameObject InputField;
+    public GameObject signUp;
+    public GameObject login;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void Retype()
+    {
+
+        InputField.SetActive(true);
+        signUp.SetActive(false);
+        login.SetActive(true);
+    }
+    public void Login()
+    {
+        signUp.SetActive(true);
+        login.SetActive(false);
+        InputField.SetActive(false);
+    }
+}
+
+
