6e77873dbfde47f5575d801232a82a04bdaf19db
YoungKrug <gregjohn456@gmail.com>
2019-08-09 12:28:12 -0400

    Added Timeline stuff
    It works!

--- drphealthmd/Assets/Scripts/GregsScripts/GameController.cs
+++ drphealthmd/Assets/Scripts/GregsScripts/GameController.cs
@@ -2,12 +2,16 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using UnityEngine.Timeline;
+
 
 public class GameController : MonoBehaviour
 {
     // Start is called before the first frame update
     [HideInInspector]
     public List<GameObject> playArea;
+    public List<GameObject> timeLines;
+    public List<GameObject> timeLineOneObjects;
     public GameObject placer;
     public GameObject planeFinder;
     public GameObject objectToSpawnOnCard;
@@ -17,6 +21,8 @@ public class GameController : MonoBehaviour
     public Text text;
     public Text debug;
     public bool isWaitingForPlayerInput = true;
+    int scenario = 0;
+    
     void Start()
     {
         image.gameObject.SetActive(false);
@@ -35,14 +41,17 @@ public class GameController : MonoBehaviour
             {
                 playArea.Add(objs[i].gameObject);
             }
+            
             button.gameObject.SetActive(false);
             text.gameObject.SetActive(false);
             image.gameObject.SetActive(false);
+            PlayScenario();
             // Deactivated the place
             // Make the outline of the cubes
             // Check if card is in the area
             placer.GetComponent<DefaultTrackableEventHandler>().enabled = false;
             planeFinder.SetActive(false);
+            //GameObject.FindObjectsOfType(typeof(Playable))
             // Now we have to track for playerInput for objects
             // We need a way to track playerMovement... Oh -Look Below
             // It will be connected to a server and wait for the teachers input to let us know
@@ -54,7 +63,7 @@ public class GameController : MonoBehaviour
        // int touches = 0;
         if(isWaitingForPlayerInput && Input.touchCount >= 1 && placer.GetComponent<DefaultTrackableEventHandler>().checkIfTracking)
             CheckIfObjectHasBeenPlaced();
-        debug.text = placer.GetComponent<DefaultTrackableEventHandler>().checkIfTracking.ToString() + '\n' + Input.touchCount.ToString();
+        debug.text = placer.GetComponent<DefaultTrackableEventHandler>().checkIfTracking.ToString() + '\n' + Input.touchCount.ToString() +"  " + timeLines[scenario].activeSelf;
        // text.gameObject.SetActive(true);
         //text.text = touches.ToString();
         //image.gameObject.SetActive(true);
@@ -73,5 +82,21 @@ public class GameController : MonoBehaviour
     public void SetIsWaitingForPlayerInput()
     {
         isWaitingForPlayerInput = false;
+    }
+    void PlayScenario()
+    {
+        if(scenario <= 1)
+        {
+            SetListEqualToTrue(timeLineOneObjects);
+        }
+        timeLines[scenario].SetActive(true);
+        scenario++;
+    }
+    void SetListEqualToTrue(List<GameObject> temp)
+    {
+        foreach(GameObject g in temp)
+        {
+            g.SetActive(true);
+        }
     }
 }
