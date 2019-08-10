80608b4d65686b21a089e35df82d0facb7d6f744
Joel Graves <jgraves1@ltu.edu>
2019-08-10 01:46:31 -0400

    Completed tHe draft of the loading screen scene

---
+++ drphealthmd/Assets/Scripts/DelayedIntro.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DelayedIntro : MonoBehaviour
+{
+    public float Elapsed = 0.0f;
+    public float timer = 5.0f;
+    public GameObject Char1;
+    public GameObject Char2;
+    public GameObject Start;
+    public GameObject loading;
+
+    // Start is called before the first frame updat
+    
+
+    // Update is called once per frame
+    void Update()
+    {
+        this.Elapsed += Time.deltaTime;
+
+        if(Elapsed > timer)
+        {
+            Appear();
+        }
+        
+    }
+
+    public void Appear()
+    {
+        Char1.SetActive(true);
+        Char2.SetActive(true);
+        Start.SetActive(true);
+        loading.SetActive(false);
+    }
+}
