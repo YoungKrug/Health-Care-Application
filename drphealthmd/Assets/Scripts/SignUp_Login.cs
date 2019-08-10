ac78d2fa682396e2cb1a3fe53aa4e4641dcf83b9
Joel Graves <jgraves1@ltu.edu>
2019-08-10 14:03:36 -0400

    finished the draft of the login screen

---
+++ drphealthmd/Assets/Scripts/SignUp_Login.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SignUp_Login : MonoBehaviour
+{
+    public GameObject retype;
+    public GameObject signUp;
+    public GameObject login;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+  
+    }
+
+    public void Toggle()
+    {
+        retype.SetActive(true);
+        login.SetActive(true);
+        signUp.SetActive(false);
+    }
+
+    public void BackToLogin()
+    {
+        retype.SetActive(false);
+        login.SetActive(false);
+        signUp.SetActive(true);
+    }
+}
