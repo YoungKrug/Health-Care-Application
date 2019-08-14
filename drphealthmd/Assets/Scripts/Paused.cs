ce0f8a25cc0c272511ae31823b6dafd6195898c2
Joel Graves <jgraves1@ltu.edu>
2019-08-14 17:43:51 -0400

    Finshed paused scene

---
+++ drphealthmd/Assets/Scripts/Paused.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Paused : MonoBehaviour
+{
+    public GameObject paused;
+    public bool isPaused = false;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    
+    public void IsPaused()
+    {
+        if(!isPaused)
+        {
+            paused.SetActive(true);
+            isPaused = true;
+            Time.timeScale = 0;
+        }
+        else
+        {
+            paused.SetActive(false);
+            isPaused = false;
+            Time.timeScale = 1;
+        }
+
+    }
+
+
+}
---
+++ drphealthmd/Assets/Scripts/Timer.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Timer : MonoBehaviour
+{
+    public float timer = 0;
+    public GameObject text;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        timer += Time.deltaTime;
+        text.GetComponent<Text>().text = timer.ToString();
+        Debug.Log(text.GetComponent<Text>().text);
+
+    }
+}
