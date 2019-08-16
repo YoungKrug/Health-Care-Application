ce78fd87fb1fb3c1e8fd340d3170e859170df4d5
YoungKrug <gregjohn456@gmail.com>
2019-08-15 20:54:09 -0400

    Added Grandma Animations and Timeline

---
+++ drphealthmd/Assets/SlowMo.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SlowMo : MonoBehaviour
+{
+    [Range(0.1f,0.99f)]
+    public float slowMotionSpeed;
+    public float slowMoTime;
+    // Start is called before the first frame update
+    void OnEnable()
+    {
+        StartCoroutine(SlowMotion());
+    }
+
+    IEnumerator SlowMotion()
+    {
+        Time.timeScale = slowMotionSpeed;
+        yield return new WaitForSeconds(slowMoTime);
+        Time.timeScale = 1f;
+    }
+}
