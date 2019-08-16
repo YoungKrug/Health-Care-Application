de2c506b5412d5b4928eda0743dac3b873192b15
Joel Graves <jgraves1@ltu.edu>
2019-08-16 07:35:29 -0400

    Finished the Character Selection Screen

---
+++ drphealthmd/Assets/Scripts/Delay.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Delay : MonoBehaviour
+{
+    public GameObject ui;
+    public float elapsed = 0f;
+    public float time = 3.3f;
+    public bool stop = false;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(stop == false)
+        {
+            elapsed += Time.deltaTime;
+            if(elapsed> time)
+            {
+                stop = true;
+                ui.SetActive(true);
+            }
+        }
+    }
+}
---
+++ drphealthmd/Assets/Scripts/OnSelectedSally.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.Playables;
+
+public class OnSelectedSally : MonoBehaviour
+{
+
+    public GameObject selectedText;
+    public GameObject Ui;
+    public GameObject cam1;
+    public GameObject cam2;
+    public PlayableAsset select;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+       
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    public void SallySelectd()
+    {
+        selectedText.GetComponent<Text>().text = "Sally Is Selected";
+    }
+    public void AJSelected()
+    {
+        selectedText.GetComponent<Text>().text = "AJ Is Selected";
+    }
+    public void IsSelected()
+    {
+        if (selectedText.GetComponent<Text>().text != "")
+        {
+            cam1.SetActive(false);
+            cam2.SetActive(true);
+            Ui.SetActive(false);
+
+        }
+        
+    }
+    
+}
+
