e90463d1d58ac43d400357a6537c7f98be61615f
YoungKrug <gregjohn456@gmail.com>
2019-10-05 08:08:08 -0400

    Getting closer

--- drphealthmd/Assets/Scripts/GregsScripts/LoginAuth.cs
+++ drphealthmd/Assets/Scripts/GregsScripts/LoginAuth.cs
@@ -23,41 +23,45 @@ public class LoginAuth : MonoBehaviour
     User[] currentUsers;
     public Text text;
     FirebaseApp app;
+    DependencyStatus dependencyStatus;
     // Start is called before the first frame update
     void Start()
     {
-        Firebase.FirebaseApp.CheckAndFixDependenciesAsync().ContinueWith(task => {
-            var dependencyStatus = task.Result;
-            if (dependencyStatus == Firebase.DependencyStatus.Available)
-            {
-                // Create and hold a reference to your FirebaseApp,
-                // where app is a Firebase.FirebaseApp property of your application class.
-                  app = Firebase.FirebaseApp.DefaultInstance;
-                InitializeFirebase(app);
-                // Set a flag here to indicate whether Firebase is ready to use by your app.
-            }
-            else
-            {
-                UnityEngine.Debug.LogError(System.String.Format(
-                  "Could not resolve all Firebase dependencies: {0}", dependencyStatus));
-                // Firebase Unity SDK is not safe to use here.
-            }
-        });
-
+        //Firebase.FirebaseApp.CheckAndFixDependenciesAsync().ContinueWith(task => {
+        //     dependencyStatus = task.Result;
+        //    if (dependencyStatus == Firebase.DependencyStatus.Available)
+        //    {
+        //        // Create and hold a reference to your FirebaseApp,
+        //        // where app is a Firebase.FirebaseApp property of your application class.
+        //          app = Firebase.FirebaseApp.DefaultInstance;
+        //        InitializeFirebase(app);
+        //        // Set a flag here to indicate whether Firebase is ready to use by your app.
+        //    }
+        //    else
+        //    {
+        //        UnityEngine.Debug.LogError(System.String.Format(
+        //          "Could not resolve all Firebase dependencies: {0}", dependencyStatus));
+        //        // Firebase Unity SDK is not safe to use here.
+        //    }
+        //});
+        Debug.Log(dependencyStatus);
+        InitializeFirebase(app);
     }
     void InitializeFirebase(FirebaseApp app)
     {
         // Google
+#if UNITY_EDITOR
         FirebaseApp.DefaultInstance.SetEditorDatabaseUrl("https://go-health-kids.firebaseio.com/");
+        FirebaseAuth.DefaultInstance.App.SetEditorDatabaseUrl("https://go-health-kids.firebaseio.com/");
+       // app.SetEditorDatabaseUrl("https://go-health-kids.firebaseio.com/");
+#else
+        FirebaseApp.DefaultInstance.Options.DatabaseUrl = new System.Uri("https://go-health-kids.firebaseio.com/");
+#endif
         //Google says:
         //Connects to the database
-        FirebaseAuth.DefaultInstance.App.SetEditorDatabaseUrl("https://go-health-kids.firebaseio.com/");
         // FirebaseAuth.DefaultInstance.
         // app = FirebaseApp.DefaultInstance;
-        app.SetEditorDatabaseUrl("https://go-health-kids.firebaseio.com/");
         databaseReference = FirebaseDatabase.DefaultInstance.GetReferenceFromUrl("https://go-health-kids.firebaseio.com/");
-        if (app.Options.DatabaseUrl != null) 
-            app.SetEditorDatabaseUrl(app.Options.DatabaseUrl);
         databaseReference.Database.GoOnline();
         GetInformation();
 
